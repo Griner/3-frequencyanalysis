35e7e5884ff08efd4f328cdefd46e0d74a9aed5d
Griner <griner9@gmail.com>
2019-06-23 13:09:31 +0600

    add filter, return max 10 positions

--- freqanalysis.go
+++ freqanalysis.go
@@ -3,6 +3,7 @@ package freqanalysis
 import (
 	"sort"
 	"strings"
+	"unicode"
 )
 
 type WordStat struct {
@@ -16,13 +17,39 @@ func (ws WordStats) Len() int           { return len(ws) }
 func (ws WordStats) Less(i, j int) bool { return ws[i].count < ws[j].count }
 func (ws WordStats) Swap(i, j int)      { ws[i], ws[j] = ws[j], ws[i] }
 
+func filter(str string) []string {
+
+	list := []string{}
+	word := strings.Builder{}
+
+	for _, s := range str {
+		if unicode.IsLetter(s) || unicode.IsDigit(s) {
+			word.WriteRune(s)
+		} else {
+			if word.Len() > 0 {
+				list = append(list, word.String())
+				word.Reset()
+			}
+		}
+	}
+
+	if word.Len() > 0 {
+		list = append(list, word.String())
+	}
+
+	return list
+}
+
 func Freq(str string) WordStats {
 
 	statsTemp := make(map[string]int)
 
-	words := strings.Split(str, " ")
+	// words := strings.Split(str, " ")
+	words := filter(str)
+
 	for _, word := range words {
-		statsTemp[word] = statsTemp[word] + 1
+		word = strings.ToLower(word)
+		statsTemp[word] += 1
 	}
 
 	stats := make(WordStats, len(statsTemp))
@@ -34,5 +61,8 @@ func Freq(str string) WordStats {
 
 	sort.Sort(sort.Reverse(stats))
 
-	return stats
+	if len(stats) < 10 {
+		return stats
+	}
+	return stats[:10]
 }
--- freqanalysis_test.go
+++ freqanalysis_test.go
@@ -11,8 +11,25 @@ func TestFreq(t *testing.T) {
 		in  string
 		out WordStats
 	}{
+		{"aaa1  \\  bbb  \n \n \t ccc\naaa1\naaa1 bbb", WordStats{{"aaa1", 3}, {"bbb", 2}, {"ccc", 1}}},
 		{"aaa bbb ccc aaa aaa bbb", WordStats{{"aaa", 3}, {"bbb", 2}, {"ccc", 1}}},
 		{"aaa bbb aaa aaa", WordStats{{"aaa", 3}, {"bbb", 1}}},
+		{"aaa bbb aaa Aaa Ccc ccc", WordStats{{"aaa", 3}, {"ccc", 2}, {"bbb", 1}}},
+		{"A a a a A a a A b b c c C c c C c C c c D d d d D e f F f F F F F g G G g G G G g G H h h H h h h h h H H i i I J j J j K K K k k k L l l l l l l l L L l L",
+			WordStats{
+				{"l", 12},
+				{"h", 11},
+				{"c", 10},
+				{"g", 9},
+				{"a", 8},
+				{"f", 7},
+				{"k", 6},
+				{"d", 5},
+				{"j", 4},
+				{"i", 3},
+				// {"b", 2},
+				// {"e", 1},
+			}},
 	}
 
 	for _, testCase := range cases {
